a958ab92ae6aebe7f4aeb69ba99850a1cec4c3e7
Mikhail Shardin <kriptonit1988@gmail.com>
2018-09-08 22:28:54 +0500

    Поставил автофокус в поле ввода даты заезда

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 var button = document.querySelector(".hotel-search-open-button");
 var popup = document.querySelector(".hotel-search-popup");
+var form = popup.querySelector(".hotel-search-form");
+var checkInDate = form.querySelector("#check-in-date");
+var checkOutDate = form.querySelector("#check-out-date");
+var adultsQuantity = form.querySelector("#adults-quantity");
+var childrenQuantity = form.querySelector("#children-quantity");
 
 popup.classList.add("hotel-search-popup-hide");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("show");
+  if (popup.classList.contains("show")) {
+    checkInDate.focus();
+  }
 });
