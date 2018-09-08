f9fbc87a19d147b53ef8d87bf5de03ede47e00f0
Mikhail Shardin <kriptonit1988@gmail.com>
2018-09-08 23:34:21 +0500

    Добавил запись в localStorage количества взрослых и детей

--- js/script.js
+++ js/script.js
@@ -6,16 +6,32 @@ var checkOutDate = form.querySelector("#check-out-date");
 var adultsQuantity = form.querySelector("#adults-quantity");
 var childrenQuantity = form.querySelector("#children-quantity");
 
+var isStorageSupport = true;
+var adultsStorage = "";
+var childrenStorage = "";
+
+try {
+  adultsStorage = localStorage.getItem("adults");
+  childrenStorage = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 popup.classList.add("hotel-search-popup-hide");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("show");
-  if (popup.classList.contains("show")) {
-    // checkInDate.focus();
-  }
   if (popup.classList.contains("error")) {
     popup.classList.remove("error");
+  }
+  if (popup.classList.contains("show")) {
+    if (adultsStorage) {
+      adultsQuantity.value = adultsStorage;
+    }
+    if (childrenStorage) {
+      childrenQuantity.value = childrenStorage;
+    }
   }
 });
 
@@ -25,6 +41,11 @@ form.addEventListener("submit", function (evt) {
     popup.classList.remove("error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("adults", adultsQuantity.value);
+      localStorage.setItem("children", childrenQuantity.value);
+    }
   }
 });
 
